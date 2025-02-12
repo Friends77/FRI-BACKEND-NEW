4c68819f77accb4d2b5c057b9542dcc68ace5e00
Hosung <an3735297@naver.com>
2025-02-13 08:35:41 +0900

    feat : friends discovery 서비스 생성

---
+++ friends-discovery/src/main/kotlin/com/example/friendsdiscovery/FriendsDiscoveryApplication.kt
@@ -0,0 +1,11 @@
+package com.example.friendsdiscovery
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FriendsDiscoveryApplication
+
+fun main(args: Array<String>) {
+    runApplication<FriendsDiscoveryApplication>(*args)
+}
---
+++ friends-discovery/src/test/kotlin/com/example/friendsdiscovery/FriendsDiscoveryApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.friendsdiscovery
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FriendsDiscoveryApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
