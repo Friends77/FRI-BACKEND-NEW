b968cead45b1cc7040af6613c53bf5b4bfc01a77
Hosung <an3735297@naver.com>
2025-02-13 08:26:01 +0900

    feat : eureka server

---
+++ server-discovery/src/main/kotlin/com/example/serverdiscovery/ServerDiscoveryApplication.kt
@@ -0,0 +1,13 @@
+package com.example.serverdiscovery
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
+
+@SpringBootApplication
+@EnableEurekaServer
+class ServerDiscoveryApplication
+
+fun main(args: Array<String>) {
+    runApplication<ServerDiscoveryApplication>(*args)
+}
