235220ba076d2c455b7984234b0cf29e3e2c6f8d
Hosung <an3735297@naver.com>
2025-02-13 10:24:54 +0900

    feat : user-service : jpa 의존성 common 으로 이동

--- friends-user-service/build.gradle.kts
+++ friends-user-service/build.gradle.kts
@@ -2,10 +2,10 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.9.25"
-    kotlin("plugin.spring") version "1.9.25"
-    kotlin("plugin.jpa") version "1.9.25"
-    id("org.springframework.boot") version "3.4.2"
-    id("io.spring.dependency-management") version "1.1.7"
+    kotlin("plugin.spring") version "1.9.25" apply false
+    kotlin("plugin.jpa") version "1.9.25" apply false
+    id("org.springframework.boot") version "3.4.2" apply false
+    id("io.spring.dependency-management") version "1.1.7" apply false
 }
 
 allprojects {
@@ -48,9 +48,6 @@ subprojects {
         testImplementation("org.springframework.boot:spring-boot-starter-test")
         testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
         testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-
-        // jpa
-        implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     }
 }
 
