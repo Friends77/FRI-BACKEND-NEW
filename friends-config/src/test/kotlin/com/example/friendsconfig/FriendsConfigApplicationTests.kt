f8395858aa7b60478b08079572257a82b1278b54
Hosung <an3735297@naver.com>
2025-02-13 09:08:12 +0900

    feat : friends config 세팅

---
+++ friends-config/src/test/kotlin/com/example/friendsconfig/FriendsConfigApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.friendsconfig
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FriendsConfigApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
