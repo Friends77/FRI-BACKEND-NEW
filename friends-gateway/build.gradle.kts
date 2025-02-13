8359f706ab8fe444ad6ac8a53de8531135c73760
Hosung <an3735297@naver.com>
2025-02-13 20:04:21 +0900

    remove : gateway : spring-web 의존성 충돌로 인한 spring security filter 삭제

--- friends-gateway/build.gradle.kts
+++ friends-gateway/build.gradle.kts
@@ -28,9 +28,6 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
-    // spring web
-    implementation("org.springframework.boot:spring-boot-starter-web")
-
     // spring config
     implementation("org.springframework.cloud:spring-cloud-starter-config")
     // bootstrap
