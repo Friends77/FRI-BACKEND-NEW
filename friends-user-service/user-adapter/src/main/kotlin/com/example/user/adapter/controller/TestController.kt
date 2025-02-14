02b8e46f64e18dc5883c0904bba579d3ecd43400
Hosung <an3735297@naver.com>
2025-02-14 10:05:32 +0900

    refactor : user-service : 앤드포인트 재설정

--- friends-user-service/user-adapter/src/main/kotlin/com/example/user/adapter/controller/TestController.kt
+++ friends-user-service/user-adapter/src/main/kotlin/com/example/user/adapter/controller/TestController.kt
@@ -6,8 +6,18 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class TestController {
-    @GetMapping("/health")
+    @GetMapping("/api/global/health")
     fun health(): ResponseEntity<String> {
+        return ResponseEntity.ok("ok")
+    }
+
+    @GetMapping("/api/user/health")
+    fun userHealth(): ResponseEntity<String> {
+        return ResponseEntity.ok("ok")
+    }
+
+    @GetMapping("/api/admin/health")
+    fun adminHealth(): ResponseEntity<String> {
         return ResponseEntity.ok("ok")
     }
 }
