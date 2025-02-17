d08fdf5140130d09f96cadd803ab4938b5890d92
Hosung <an3735297@naver.com>
2025-02-17 09:22:37 +0900

    feat : chat-service : domain : base entity

---
+++ friends-chat-service/chat-boot/src/main/kotlin/com/example/chat/boot/ChatApplication.kt
@@ -0,0 +1,18 @@
+package com.example.chat.boot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
+import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+
+@SpringBootApplication
+@ComponentScan(basePackages = ["com.example"])
+@EnableJpaRepositories(basePackages = ["com.example"])
+@EntityScan(basePackages = ["com.example"])
+class ChatApplication {
+}
+
+fun main(args: Array<String>) {
+    runApplication<ChatApplication>(*args)
+}
---
+++ friends-chat-service/chat-domain/src/main/kotlin/com/example/chat/domain/config/JpaAuditingConfig.kt
@@ -0,0 +1,8 @@
+package com.example.chat.domain.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@Configuration
+@EnableJpaAuditing
+class JpaAuditingConfig
