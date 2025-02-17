30e320294592552e3d9ee6423870268e00e235fc
Hosung <an3735297@naver.com>
2025-02-17 16:56:08 +0900

    feat : message-controller : 프로젝트 생성

---
+++ freinds-message-controller/src/main/kotlin/com/example/freindsmessagecontroller/FreindsMessageControllerApplication.kt
@@ -0,0 +1,11 @@
+package com.example.freindsmessagecontroller
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FreindsMessageControllerApplication
+
+fun main(args: Array<String>) {
+    runApplication<FreindsMessageControllerApplication>(*args)
+}
---
+++ freinds-message-controller/src/test/kotlin/com/example/freindsmessagecontroller/FreindsMessageControllerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.freindsmessagecontroller
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FreindsMessageControllerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
