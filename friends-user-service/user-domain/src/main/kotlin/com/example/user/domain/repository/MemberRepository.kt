e7c75e4dfac5eab6b2921f507767a20a8dca9721
Hosung <an3735297@naver.com>
2025-02-21 16:20:31 +0900

    feat : user-service : domain : 도메인 레이어가 JPA 에 의존성을 갖도록 변경

--- friends-user-service/user-domain/src/main/kotlin/com/example/user/domain/repository/MemberRepository.kt
+++ friends-user-service/user-domain/src/main/kotlin/com/example/user/domain/repository/MemberRepository.kt
@@ -2,12 +2,13 @@ package com.example.user.domain.repository
 
 import com.example.user.domain.entity.Member
 import com.example.user.domain.valueobject.Email
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import java.util.UUID
 
-interface MemberRepository {
+interface MemberRepository : JpaRepository<Member, UUID> {
+    @Query("SELECT m FROM Member m LEFT JOIN FETCH m.mutableAuthorities WHERE m.email = :email")
     fun findByEmailWithAuthorities(email: Email): Member?
-
-    fun save(member: Member): Member
 
     fun existsByEmail(email: Email): Boolean
 
--- friends-user-service/user-domain/src/main/kotlin/com/example/user/domain/repository/ProfileRepository.kt
+++ friends-user-service/user-domain/src/main/kotlin/com/example/user/domain/repository/ProfileRepository.kt
@@ -1,10 +1,10 @@
 package com.example.user.domain.repository
 
 import com.example.user.domain.entity.Profile
+import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import java.util.UUID
 
-interface ProfileRepository {
-    fun save(profile: Profile): Profile
+interface ProfileRepository : JpaRepository<Profile, UUID> {
     fun findByMemberId(memberId: UUID): Profile?
 }
