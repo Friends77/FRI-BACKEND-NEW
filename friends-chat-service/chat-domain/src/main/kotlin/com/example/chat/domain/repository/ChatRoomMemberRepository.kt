0665d680284775d69d1e385751ef9f1e8b874f7c
Hosung <an3735297@naver.com>
2025-02-24 14:53:17 +0900

    fix : chat-service : repo : 참여중인 채팅방 조회

--- friends-chat-service/chat-domain/src/main/kotlin/com/example/chat/domain/repository/ChatRoomMemberRepository.kt
+++ friends-chat-service/chat-domain/src/main/kotlin/com/example/chat/domain/repository/ChatRoomMemberRepository.kt
@@ -9,6 +9,6 @@ interface ChatRoomMemberRepository : JpaRepository<ChatRoomMember, UUID> {
     @Query("SELECT crm FROM ChatRoomMember crm WHERE crm.memberId = :memberId AND crm.isCustomProfile = false")
     fun findAllByMemberIdHavingNotCustomProfile(memberId: UUID): List<ChatRoomMember>
 
-    @Query("SELECT c.id FROM ChatRoomMember c WHERE c.memberId = :memberId")
-    fun findAllIdByMemberId(memberId: UUID): List<UUID>
+    @Query("SELECT c.chatRoom.id FROM ChatRoomMember c WHERE c.memberId = :memberId")
+    fun findAllChatRoomIdByMemberId(memberId: UUID): List<UUID>
 }
